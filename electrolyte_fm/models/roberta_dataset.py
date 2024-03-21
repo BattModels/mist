eb5c01a41e5c714b70bd52a9d206d8f560e8b2c0
Alexius Wadell <awadell@gmail.com>
2024-03-21 14:38:37 +0000

    tweak: use tokenizer parallelism

--- electrolyte_fm/models/roberta_dataset.py
+++ electrolyte_fm/models/roberta_dataset.py
@@ -19,7 +19,7 @@ class RobertaDataSet(pl.LightningDataModule):
         mlm_probability=0.15,
         batch_size: int = 64,
         val_batch_size=None,
-        num_workers=8,
+        num_workers=1,
         prefetch_factor=4,
     ):
         super().__init__()
