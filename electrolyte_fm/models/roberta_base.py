2bbeea4a2d2b155361edab2b7128633a151d2b30
Changwen Xu <changwex@umich.edu>
2024-05-21 03:03:00 +0000

    Update roberta_base.py with muP

--- electrolyte_fm/models/roberta_base.py
+++ electrolyte_fm/models/roberta_base.py
@@ -5,6 +5,8 @@ from transformers import RobertaConfig, RobertaForMaskedLM
 
 from .model_utils import DeepSpeedMixin, LoggingMixin
 
+from mup import make_base_shapes, set_base_shapes, MuAdamW
+
 
 class RoBERTa(DeepSpeedMixin, LoggingMixin):
     """
@@ -19,7 +21,16 @@ class RoBERTa(DeepSpeedMixin, LoggingMixin):
         num_attention_heads: int = 12,
         num_hidden_layers: int = 6,
         hidden_size: int = 768,
-        optimizer: OptimizerCallable = torch.optim.AdamW,
+        base_intermediate_size: int = 256,
+        base_num_attention_heads: int = 4,
+        base_num_hidden_layers: int = 3,
+        base_hidden_size: int = 256,
+        delta_intermediate_size: int = 300,
+        delta_num_attention_heads: int = 5,
+        delta_num_hidden_layers: int = 2,
+        delta_hidden_size: int = 200,
+        optimizer: OptimizerCallable = MuAdamW,
+        base_shape_save_path:str = 'base_shape/bert256.bsh',
         lr_schedule: LRSchedulerCallable | None = None,
     ) -> None:
         super().__init__()
@@ -27,6 +38,36 @@ class RoBERTa(DeepSpeedMixin, LoggingMixin):
         self.lr_schedule = lr_schedule
         self.vocab_size = vocab_size
         self.save_hyperparameters(ignore=["optimizer", "lr_schedule"])
+
+        # Define base and delta model
+        self.base_config = RobertaConfig(
+            vocab_size=vocab_size,
+            intermediate_size=base_intermediate_size,
+            hidden_size=base_hidden_size,
+            max_position_embeddings=max_position_embeddings,
+            num_attention_heads=base_num_attention_heads,
+            num_hidden_layers=base_num_hidden_layers,
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
+            type_vocab_size=1,
+        )
+        self.base_model = RobertaForMaskedLM(config=self.base_config)
+
+        self.delta_config = RobertaConfig(
+            vocab_size=vocab_size,
+            intermediate_size=delta_intermediate_size,
+            hidden_size=delta_hidden_size,
+            max_position_embeddings=max_position_embeddings,
+            num_attention_heads=delta_num_attention_heads,
+            num_hidden_layers=delta_num_hidden_layers,
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
+            type_vocab_size=1,
+        )
+        self.delta_model = RobertaForMaskedLM(config=self.delta_config)
+
+        # define a base shape object based on comparing delta_model against base_model
+        base_shapes = make_base_shapes(self.base_model, self.delta_model, savefile=base_shape_save_path)
 
         self.config = RobertaConfig(
             vocab_size=vocab_size,
@@ -40,6 +81,12 @@ class RoBERTa(DeepSpeedMixin, LoggingMixin):
             type_vocab_size=1,
         )
         self.model = RobertaForMaskedLM(config=self.config)
+
+        # set base shapes
+        set_base_shapes(self.model, base_shapes)
+
+        # re-initialize
+        self.model.apply(self.model._init_weights)
 
     def get_encoder(self):
         return self.model.roberta
