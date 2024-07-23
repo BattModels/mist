005f4d50cc6cffd9ec0bc5dfbf256e5f90b1ccb9
Anoushka Bhutani <abhutani@umich.edu>
2024-07-23 18:51:44 -0400

    fix: remove unused imports

--- electrolyte_fm/utils/callbacks/gradient_norm_monitor.py
+++ electrolyte_fm/utils/callbacks/gradient_norm_monitor.py
@@ -1,21 +1,10 @@
-import json
-import os
-import time
-from tabnanny import check
-from typing import Any, Mapping, Union
-
 import pytorch_lightning as pl
 from deepspeed.utils import safe_get_full_grad
 from pytorch_lightning.callbacks import Callback
-from pytorch_lightning.loggers import WandbLogger
-from typing_extensions import override
-
-from ...models.model_utils import DeepSpeedMixin
-from ..ckpt import SaveConfigWithCkpts
 
 
 class GradientNormMonitor(Callback):
-    """Custom callback in order to monitor the gradient norm and log to weights and biases."""
+    """Custom callback in order to monitor and log the gradient norm."""
 
     def __init__(self) -> None:
         """Logs throughput statistics starting at the 2nd epoch."""
@@ -25,7 +14,7 @@ class GradientNormMonitor(Callback):
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
         # Compute the 2-norm for each layer
-        # If using mixed precision, the gradients are already unscaled here
+        # If using mixed precision, the gradients are unscaled here
         norms = {
             f"grad_norm/{n}": safe_get_full_grad(p).norm(2)
             for n, p in pl_module.model.named_parameters()
