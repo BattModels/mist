ab9cc0a9abab9b1e17c2dca0f3928d51b45e4264
Alexius Wadell <awadell@gmail.com>
2024-08-11 14:29:46 -0400

    test: add test for strip_unk_tokens

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -3,7 +3,9 @@ import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from pytorch_lightning import Trainer, LightningDataModule
-from electrolyte_fm.data_modules import RobertaDataSet
+from electrolyte_fm.data_modules import RobertaDataSet, PropertyPredictionDataModule
+from electrolyte_fm.data_modules.property_prediction_dataset import strip_unk_tokens
+from electrolyte_fm.utils.tokenizer import load_tokenizer
 
 
 def check_datamodule(dm: LightningDataModule, stage="fit", limit_batches=100):
@@ -47,3 +49,28 @@ def test_canonical_dataset(fake_dataset):
     dm = RobertaDataSet(fake_dataset, "smirk", canonical=True)
     assert dm.canonical
     check_datamodule(dm)
+
+
+def test_property_dataset():
+    dm = PropertyPredictionDataModule(
+        "/nfs/turbo/coe-venkvis/mist/molformer_ft_full/freesolv",
+        "ibm/MoLFormer-XL-both-10pct-oov",
+        target_columns=["calc"],
+    )
+    dm.prepare_data()
+    dm.setup("fit")
+    check_dataloader(dm.train_dataloader(), 5)
+
+
+def test_strip_unknown_tokens():
+    tok = load_tokenizer("ibm/MoLFormer-XL-both-10pct-oov")
+    encoding = tok("🚀C1N(CN(CN1[N+](=O)[O-])[N+](=O)[O-])[N+](=O)[O-]")
+    assert tok.unk_token_id in encoding["input_ids"]
+    assert len(tok.unk_token_id) == 33
+    strip_encoding = strip_unk_tokens(encoding)
+    assert "input_ids" in strip_encoding
+    assert "attention_mask" in strip_encoding
+    assert tok.unk_token_id not in strip_encoding["input_ids"]
+    assert len(strip_encoding["input_ids"]) == 32
+    for k, v in strip_encoding.items():
+        assert len(v) == 32
