7ba3a653a19f30c71c730d7d1992a18b1b2bac5d
Alexius Wadell <awadell@gmail.com>
2024-08-14 15:38:36 -0400

    feat: sync bootstrap stats

--- opt/sync_wandb.py
+++ opt/sync_wandb.py
@@ -1,10 +1,11 @@
 import json
 import re
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 import logging
 
 import wandb
+from datasets import fingerprint
 
 logging.basicConfig(level=logging.INFO)
 
@@ -59,6 +60,7 @@ def run_summary(run):
         "name": run.name,
         "url": run.url,
         "tags": run.tags,
+        "state": run.state,
         "hostname": get_cluster(run.metadata["host"]),
         "created": run.metadata["startedAt"],
         "steps": run.summary["trainer/global_step"],
@@ -107,9 +109,6 @@ def pretraining_summary(run):
     return row
 
 
-METRIC_REGEX = re.compile(r"(?P<split>\w+)/(?P<metric>[a-zA-Z0-9]+)_(?P<tok_group>\w+)")
-
-
 def finetuning_summary(run):
     row = run_summary(run)
     config = run.config
@@ -131,43 +130,66 @@ def finetuning_summary(run):
     row["encoder_id"] = encoder_id
 
     # Identify metrics
-    summary_metrics = set()
-    for k in run.summary.keys():
-        if m := METRIC_REGEX.match(k):
-            if m.group("tok_group") in ["oov", "non_oov", "all"]:
-                summary_metrics.add(m.group("metric"))
-
-    metrics = []
-    for metric in summary_metrics:
-        for split in ["train", "val", "test"]:
-            for tok_group in ["oov", "non_oov", "all"]:
-                try:
-                    metrics.append(
-                        {
-                            "split": split,
-                            "metric": metric,
-                            "token_group": tok_group,
-                            "best": run.summary_metrics[
-                                f"{split}/{metric}_{tok_group}"
-                            ]["best"],
-                        }
-                    )
-                except KeyError:
-                    pass
-    row["metrics"] = metrics
+    row["metrics"] = identify_metrics(run.summary_metrics)
     return row
+
+
+METRIC_REGEX = re.compile(
+    r"(?P<split>\w+)/(?P<metric>[a-zA-Z0-9]+)_(?P<tok_group>(?:oov)|(?:all)|(?:non_oov))(?:_(?P<bootstrap>(?:mean)|(?:std)))?"
+)
+
+
+def identify_metrics(summary_metrics):
+    metrics = []
+    for k, v in summary_metrics.items():
+        m = METRIC_REGEX.match(k)
+        if m is None:
+            continue
+
+        # Unpack metric
+        entry = m.groupdict()
+        if isinstance(v, (float, int)):
+            entry["type"] = "last"
+            entry["value"] = v
+            metrics.append(entry)
+        else:
+            # Multiple summary metrics were logged
+            for sk, sv in v.items():
+                metrics.append({"type": sk, "value": sv, **entry})
+    return metrics
 
 
 def export_runs(exportfun, cache: Path, runs, name: str = None):
     if name:
         cache = cache_path.joinpath(name)
         cache.mkdir(exist_ok=True, parents=True)
+    else:
+        name = "default"
     for run in runs:
         run_cache = cache.joinpath(run.id).with_suffix(".json")
+
+        # Compute fingerprint
+        fp = fingerprint.update_fingerprint(
+            run.id,
+            exportfun,
+            {"run": run.id, "entity": run.entity, "project": run.project},
+        )
+
+        # Check for an existing export
         if run_cache.exists():
-            continue
-        logging.info("exporting %s (%s) to %s", run.id, run.url, name or "default")
+            with open(run_cache, "r") as fid:
+                export_fp = json.load(fid).get("fingerprint", None)
+
+            # If the fingerprint hasn't changed, don't need to export again
+            if export_fp == fp:
+                logging.info(
+                    "fingerprint for %s/%s matches (%s), skipping", name, run.id, fp
+                )
+                continue
+
+        logging.info("exporting %s/%s (%s)", name, run.id, run.url)
         stats = exportfun(run)
+        stats["fingerprint"] = fp
         with open(run_cache, "w") as fid:
             json.dump(stats, fid)
 
@@ -183,23 +205,24 @@ if __name__ == "__main__":
     runs = api.runs(
         "incite-mist/mist",
         filters={
-            "State": {"$in": ["Crashed", "Finished"]},
+            "State": {"$in": ["Failed", "Finished"]},
             "tags": {"$in": ["pretraining"], "$nin": ["debug"]},
             "summary_metrics.trainer/global_step": {"$exists": True, "$gte": 100},
             "summary_metrics.val/loss_epoch.min": {"$exists": True},
         },
     )
-    export_runs(pretraining_summary, cache_path, runs, name="pretraining")
+    # export_runs(pretraining_summary, cache_path, runs, name="pretraining")
 
     # Sync Finetuning
     runs = api.runs(
         "incite-mist/mist",
         filters={
-            "State": {"$in": ["Crashed", "Finished"]},
+            "State": {"$in": ["Failed", "Finished"]},
             "tags": {"$in": ["finetuning"], "$nin": ["debug"]},
             "summary_metrics.trainer/global_step": {"$exists": True, "$gte": 100},
-            # "summary_metrics.val/loss_epoch.min": {"$exists": True},
+            "summary_metrics.val/loss_epoch": {"$exists": True},
             "config.cli.model.init_args.encoder_ckpt": {"$exists": True},
+            # "config.cli.data.init_args.name": {"$exists": True},
         },
     )
     export_runs(finetuning_summary, cache_path, runs, name="finetuning")
