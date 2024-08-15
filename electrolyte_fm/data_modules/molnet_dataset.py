92ca91e56fdb97ddc0d5451407353035c62673cc
Alexius Wadell <awadell@gmail.com>
2024-08-15 11:16:01 -0400

    fix-deps: missing rdkit, rm unused xxhash

--- electrolyte_fm/data_modules/molnet_dataset.py
+++ electrolyte_fm/data_modules/molnet_dataset.py
@@ -4,7 +4,6 @@ from statistics import mean
 from typing import Dict, List, Optional, Union
 import logging
 
-from xxhash import xxh64
 from rdkit.Chem.Scaffolds.MurckoScaffold import MurckoScaffoldSmiles
 from sklearn.model_selection import GroupShuffleSplit
 import pytorch_lightning as pl
