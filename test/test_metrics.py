f0c21eeccf1a6bf6f5f0a6f3ce0e171eec59f61f
Alexius Wadell <awadell@gmail.com>
2024-08-15 15:58:20 -0400

    formatting: whitespace

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -153,7 +153,9 @@ def test_auroc():
 
 
 def test_oov_metric():
-    metric = OOVMetric(Accuracy(task="binary", ignore_index=IGNORE_INDEX), unk_token_id=1)
+    metric = OOVMetric(
+        Accuracy(task="binary", ignore_index=IGNORE_INDEX), unk_token_id=1
+    )
     out = metric(
         torch.tensor([True, False, False]),
         torch.tensor([False, True, False]),
@@ -163,6 +165,7 @@ def test_oov_metric():
     assert out["oov"] == torch.tensor(0)
     assert out["non_oov"] == torch.tensor(0.5)
     assert out["all"] == torch.tensor(1 / 3)
+
 
 @pytest.mark.parametrize(
     "name,task_type",
@@ -180,22 +183,23 @@ def test_oov_metric_empty_group(name, task_type):
     # Check for all non_oov
     input_ids = torch.zeros((8, 8))
     out = metric(preds, targets, input_ids)
-    assert isinstance(out, dict) 
+    assert isinstance(out, dict)
     if hasattr(base_metric, "keys"):
         for k in base_metric:
             assert f"{k}_oov" in out and f"{k}_non_oov" in out and f"{k}_all" in out
     else:
-        assert "oov" in out and "non_oov" in out and "all" in out 
+        assert "oov" in out and "non_oov" in out and "all" in out
 
     # Check for all oov
     input_ids = torch.ones((8, 8))
     out = metric(preds, targets, input_ids)
-    assert isinstance(out, dict) 
+    assert isinstance(out, dict)
     if hasattr(base_metric, "keys"):
         for k in base_metric:
             assert f"{k}_oov" in out and f"{k}_non_oov" in out and f"{k}_all" in out
     else:
-        assert "oov" in out and "non_oov" in out and "all" in out 
+        assert "oov" in out and "non_oov" in out and "all" in out
+
 
 def test_oov_metric_is_oov():
     metric = OOVMetric(Accuracy(task="binary"), unk_token_id=1)
