f264db4700a618d7ae9809e9fff5bd8d88010dda
Alexius Wadell <awadell@gmail.com>
2024-08-23 15:54:27 -0400

    fix: missing import

--- electrolyte_fm/utils/tokenizer.py
+++ electrolyte_fm/utils/tokenizer.py
@@ -83,10 +83,13 @@ def load_tokenizer(name: str, **kwargs) -> PreTrainedTokenizerBase:
         return regex_smiles_tokenizer(vocab_from_words(vocab_file), regex)
 
     elif name.startswith("devalab/molgpt"):
+        from .cache import cached_github_archive
+
         # Source: https://github.com/devalab/molgpt/blob/72ff33ae747c0a4908b822732019a66e965a595a/train/train.py#L96C15-L96C122
         regex = r"(\[[^\]]+]|<|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
 
-        repo_ver = ("devalab/molgpt", "72ff33ae747c0a4908b822732019a66e965a595a")
+        repo_ver = ("devalab/molgpt",
+                    "72ff33ae747c0a4908b822732019a66e965a595a")
         if name.endswith("moses"):
             file = cached_github_archive(*repo_ver, "moses2_stoi.json")
         else:
@@ -143,7 +146,8 @@ def load_tokenizer(name: str, **kwargs) -> PreTrainedTokenizerBase:
         )
         config = json.loads(tok_tf.backend_tokenizer.to_str())
         regex = config["pre_tokenizer"]["pretokenizers"][-1]["pattern"]["Regex"]
-        tok_tf.backend_tokenizer.pre_tokenizer = Split(Regex(regex), "isolated")
+        tok_tf.backend_tokenizer.pre_tokenizer = Split(
+            Regex(regex), "isolated")
         return tok_tf
 
     elif (
@@ -173,7 +177,8 @@ def load_tokenizer(name: str, **kwargs) -> PreTrainedTokenizerBase:
         )
         config = json.loads(tok_tf.backend_tokenizer.to_str())
         regex = config["pre_tokenizer"]["pretokenizers"][-1]["pattern"]["Regex"]
-        tok_tf.backend_tokenizer.pre_tokenizer = Split(Regex(regex), "isolated")
+        tok_tf.backend_tokenizer.pre_tokenizer = Split(
+            Regex(regex), "isolated")
         return tok_tf
 
     elif name == "character":
