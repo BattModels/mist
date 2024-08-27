bb0cb644aa43b821622fab7fbe683bafc1411440
Anoushka Bhutani <anoushkb@andrew.cmu.edu>
2024-08-27 14:26:20 +0000

    feat: pbs requeue method for polaris added to requeue callback

--- electrolyte_fm/utils/callbacks/requeue.py
+++ electrolyte_fm/utils/callbacks/requeue.py
@@ -2,7 +2,7 @@ import os
 import sys
 import signal
 import logging
-from subprocess import run
+from subprocess import PIPE, run, Popen, STDOUT
 from pathlib import Path
 from shutil import which
 from typing import Optional, Union
@@ -19,7 +19,7 @@ log = logging.getLogger(__name__)
 
 class Requeue(Callback):
     def __init__(self, requeue_signal: Optional[signal.Signals] = None):
-        requeue_signal = requeue_signal or signal.SIGUSR1
+        requeue_signal = requeue_signal or signal.SIGTERM
         self.signal = signal.Signals(requeue_signal)
         self.ckpt_path: Optional[Path] = None
         self.requeue_count: int = 0
@@ -29,7 +29,7 @@ class Requeue(Callback):
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
         self.trainer = trainer
-        self.ckpt_path = SaveConfigWithCkpts.log_dir(trainer)
+        self.ckpt_path = SaveConfigWithCkpts.log_dir(trainer).joinpath("checkpoints")
         signal.signal(self.signal, self.handle_requeue_signal)
         log.info("Registered handler for %s", self.signal.name)
 
@@ -47,6 +47,8 @@ class Requeue(Callback):
     def requeue(self, ckpt_path: Path):
         if which("scontrol"):
             self._requeue_slurm(ckpt_path)
+        if which("qrerun"):
+            self._requeue_pbs(ckpt_path)
         else:
             raise RuntimeError("Unknown HPC Environment -> Failed to requeue")
 
@@ -65,6 +67,31 @@ class Requeue(Callback):
         os.symlink(ckpt_path.resolve(), requeue_marker)
 
         run(["scontrol", "requeue", job_id], check=True)
+
+    def _requeue_pbs(self, ckpt_path: Path):
+        if job_array_id := os.environ.get("PBS_ARRAY_ID", None):
+            job_id = job_array_id + "_" + os.environ["PBS_ARRAY_INDEX"]
+        else:
+            job_id = os.environ["PBS_JOBID"]
+
+        requeue_marker = ckpt_path.parent.parent.joinpath("requeue", job_id)
+        requeue_marker.parent.mkdir(exist_ok=True, parents=True)
+        requeue_marker.unlink(missing_ok=True)
+        os.symlink(ckpt_path.resolve(), requeue_marker)
+
+        script = Popen(
+            ["submit/resubmit", "submit/polaris.j2", ckpt_path, "--resume-wandb"],
+            stdout=PIPE,
+            stderr=STDOUT,
+        )
+        submit = Popen(["qsub"], stdin=script.stdout, stdout=PIPE, stderr=STDOUT)
+        script.stdout.close()
+
+        output, errors = submit.communicate()
+
+        log.info(output.decode())
+        if errors:
+            log.error(errors.decode())
 
     def state_dict(self):
         return {
