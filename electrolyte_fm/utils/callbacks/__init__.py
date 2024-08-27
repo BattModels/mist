d139beaf8e6dac19b4017bdbd2ef6d24f7a91122
Anoushka Bhutani <anoushkb@andrew.cmu.edu>
2024-08-27 19:58:38 +0000

    fix: LLMPredictions import in callbacks

--- electrolyte_fm/utils/callbacks/__init__.py
+++ electrolyte_fm/utils/callbacks/__init__.py
@@ -1,5 +1,4 @@
 from .gradient_norm_monitor import GradientNormMonitor
 from .spike_detection import SpikeDetection
 from .throughput_monitor import ThroughputMonitor
-from .logging import LLMPredictions
 from .requeue import Requeue
