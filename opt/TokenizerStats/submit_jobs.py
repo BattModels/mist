7309b9be3b396f2dfc5e72e7351bda114eb590d3
Alexius Wadell <awadell@gmail.com>
2024-08-30 13:12:02 -0400

    tweak: update submit_jobs.py

--- opt/TokenizerStats/submit_jobs.py
+++ opt/TokenizerStats/submit_jobs.py
@@ -51,54 +51,66 @@ TOKENIZERS = [
     "meta-llama/Meta-Llama-3-8B",
 ]
 
+REF_INFO_LOSS = [
+    "character",
+    "meta-llama/Meta-Llama-3.1-8B",
+    "google/gemma-7b",
+    "Xenova/gpt-4o",
+]
 
-def sbatch(args: list, output: Optional[str] = None, test=False) -> Optional[int]:
+
+def sbatch(args: list, output: Optional[str] = None, test=True) -> Optional[int]:
     args = [str(x) for x in args]
     if output is not None and Path(output).exists():
-        # logging.info("skipping job for %s", output)
+        logging.info("skipping job for %s", output)
         return None
+
     logging.info("submit sbatch: %s => %s", args, output)
     if test:
         return randint(0, 100)
 
     out = subprocess.run(
-        args,
-        executable="sbatch",
+        ["/usr/bin/sbatch", *args],
         text=True,
         capture_output=True,
-        check=True,
     )
+    if out.returncode != 0:
+        raise RuntimeError(f"sbatch failed: {out.stderr}")
+
     return int(out.stdout.split(" ")[-1])
 
 
 def sub_realspace(tok):
     tok_name = Path(tok).name if Path(tok).is_dir() else tok
     out = STATS_DIR.joinpath(tok_name, "realspace_v4_dev.bson")
-    id = sbatch(
-        [
-            "--time=1-0:0:0",
-            "--ntasks=32",  # Needs to divide datasets evenly (big perf. hit otherwise)
-            "--cpus-per-task=1",
-            "submit_tok_stats.sh",
-            "usage",
-            REALSPACE,
-            tok,
-        ],
-        out,
-    )
+    args = [
+        "--cpus-per-task=1",
+        "--time=1-0:0:0",
+        "--ntasks=32",  # Needs to divide datasets evenly (big perf. hit otherwise)
+        "submit_tok_stats.sh",
+        "usage",
+        "--splits=all",
+        REALSPACE,
+        tok,
+    ]
+    args = special_case(tok_name, args)
+
+    # id = sbatch(args, out)
+    id = None
     return tok_name, id, out
 
 
-_, char_id, char_realspace = sub_realspace("character")
+def special_case(tok_name, args):
+    if tok_name == "SmilesPE/SPE_ChEMBL":
+        args.insert(0, "--mem-per-cpu=4G")
+        args.insert(0, "--partition=venkvis-largemem")
+    return args
+
 
 for tok in TOKENIZERS:
     # Submit realspace_v4_dev job
-    if tok != "character":
-        tok_name, id, file = sub_realspace(tok)
-    else:
-        tok_name = "character"
-        id = char_id
-        file = char_realspace
+    tok_name, id, realspace_file = sub_realspace(tok)
+    ngram_name = realspace_file.with_suffix("").name
 
     # Compute realspace model loss
     for ds in MOLNET_DATASETS:
@@ -108,32 +120,39 @@ for tok in TOKENIZERS:
             "--cpus-per-task=1",
             "submit_tok_stats.sh",
             "loss",
-            file,
+            realspace_file,
             ds,
         ]
         if id:
             args.insert(0, f"-d=afterok:{id}")
-        sbatch(args, STATS_DIR.joinpath(tok, f"{ds}_model_loss.bson"))
+        args = special_case(tok_name, args)
+        if realspace_file.exists():
+            sbatch(args, STATS_DIR.joinpath(tok, ds, f"{ngram_name}_model_loss.bson"))
 
         # Process molnets datsets too
-        sbatch(
-            ["-n=1", "submit_tok_stats.sh", "usage", ds, tok],
-            STATS_DIR.joinpath(tok_name, f"{ds}.bson"),
-        )
+        args = ["--ntasks=1", "--time=1:0:0", "submit_tok_stats.sh", "usage", ds, tok]
+        args = special_case(tok_name, args)
+        sbatch(args, STATS_DIR.joinpath(tok_name, f"{ds}.bson"))
 
         # Compute Info loss
-        args = [
-            "--ntasks=48",
-            "--cpus-per-task=1",
-            "--time=1-0:0:0",
-            "submit_tok_stats.sh",
-            "distortion",
-            f"--reference={char_realspace}",
-            ds,
-            tok,
-        ]
-        if char_id:
-            args.insert(0, f"-d=afterok:{char_id}")
+        for ref in REF_INFO_LOSS:
+            ref_file = STATS_DIR.joinpath(ref, "realspace_v4_dev.bson")
+            if not ref_file.exists():
+                continue
 
-        if tok != "character":
-            sbatch(args, STATS_DIR.joinpath(tok_name, f"{ds}_character_info_loss.bson"))
+            args = [
+                "--ntasks=48",
+                "--cpus-per-task=1",
+                "--time=1-0:0:0",
+                "submit_tok_stats.sh",
+                "distortion",
+                f"--reference={ref_file}",
+                ds,
+                tok,
+            ]
+            args = special_case(tok_name, args)
+            if tok != ref:
+                ref_name = ref.replace("/", "--")
+                sbatch(
+                    args, STATS_DIR.joinpath(tok_name, ds, f"{ref_name}_info_loss.bson")
+                )
