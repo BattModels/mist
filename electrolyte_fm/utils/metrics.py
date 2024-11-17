e22967ffd40ef05bd81081b321a0a76784543136
Anoushka Bhutani <abhutani@umich.edu>
2024-11-17 17:55:15 -0500

    fix: formatting

--- electrolyte_fm/utils/metrics.py
+++ electrolyte_fm/utils/metrics.py
@@ -80,9 +80,11 @@ class MeanAbsoluteError(Metric):
         out = dict(zip(self.target_labels, out))
         out["mean"] = self.sum_abs_error.mean() / self.total
         return out
-    
+
     def keys(self):
-        keys_ = ["mean", ]
+        keys_ = [
+            "mean",
+        ]
         keys_.extend(self.target_labels)
         return keys_
 
