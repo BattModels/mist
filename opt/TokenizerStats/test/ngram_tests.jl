4070ee1528e4fb1711b3b24e7719475d16cfa312
Alexius Wadell <awadell@gmail.com>
2024-12-07 19:49:47 -0500

    fix: missing edge case in align_unknown

--- opt/TokenizerStats/test/ngram_tests.jl
+++ opt/TokenizerStats/test/ngram_tests.jl
@@ -215,6 +215,12 @@ end
     @testset "mismatched" begin
         @test_throws ErrorException align_unknown(["hello", "world"], ["hello", "foo"])
     end
+    @testset "false alignment" begin
+        ref = vec("C[Ni@TB12]123") # Fragment of a real SMILES from tmQM
+        smi = ["C", "[UNK]", "1", "2", "3"]
+        out = check_commutative(ref, smi)
+        @test out == sparse([1, 11, 12], [1, 3, 4], trues(3), length(ref), length(smi))
+    end
 end
 
 @testitem "Rm Special Tokens" begin
