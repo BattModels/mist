5b359dca020407bf1709460c309688e48f87f6dd
Alexius Wadell <awadell@gmail.com>
2025-03-03 12:38:28 -0800

    wip: rework sae to inject coders into the model

--- electrolyte_fm/data_modules/__init__.py
+++ electrolyte_fm/data_modules/__init__.py
@@ -1,5 +1,4 @@
 from .molnet_dataset import MolNetDataModule
 from .property_prediction_dataset import PropertyPredictionDataModule
 from .roberta_dataset import RobertaDataSet
-from .sae_dataset import HiddenStateDataModule
 from .tmqm_dataset import tmQMDataModule
--- electrolyte_fm/models/__init__.py
+++ electrolyte_fm/models/__init__.py
@@ -3,4 +3,4 @@ from .prod_finetune import MISTFinetuned, MISTMultiTask
 from .roberta_base import RoBERTa
 from .roberta_prelayernorm import RoBERTaPreLayerNorm
 from .roformer_base import RoFormer
-from .sae import SAE
+from .sae import LightningSAE
