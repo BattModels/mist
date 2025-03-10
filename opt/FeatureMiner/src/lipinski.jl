41f01340afbe88f5b64721e5200b16b7b2376978
Alexius Wadell <awadell@gmail.com>
2025-03-10 12:33:17 -0700

    add layerwise similarity and submission script

--- opt/FeatureMiner/src/lipinski.jl
+++ opt/FeatureMiner/src/lipinski.jl
@@ -1,23 +1,46 @@
 function load_fitted_probes(ckpt_dir)
     probes = []
+    local ckpt_meta
     for ckpt in readdir(ckpt_dir; join=true)
-        startswith(basename(ckpt), "star") || continue
+        @info realpath(ckpt)
+        if !startswith(basename(ckpt), "star")
+            _, ckpt_meta = load_linear_probes(ckpt)
+            continue
+        end
         m = match(r"layer-(\d+)-(\w+).*?--auroc-([\d\.]+)\.ckpt", basename(ckpt))
         layer = parse(Int, m[1]) + 1
         location = m[2]
         auroc = parse(Float64, m[3])
-        ckpt_probes, ckpt_meta = load_linear_probes(ckpt)
-        push!(probes, (;
-            ckpt_probes[layer]...,
-            model=ckpt_meta.name_or_path,
-            auroc,
-        ))
+        try
+            ckpt_probes, ckpt_meta = load_linear_probes(ckpt)
+            push!(probes, (;
+                ckpt_probes[layer]...,
+                location,
+                auroc,
+            ))
+        catch e
+            e isa InterruptException && rethrow()
+            @error "failed to load $ckpt" e catch_backtrace()
+            continue
+        end
     end
-    return DataFrame(probes)
+    return DataFrame(probes), ckpt_meta
 end
+
+cosine_similarity(a::Vector, b::Vector) = dot(a, b) / (norm(a) * norm(b))
 
 function additive_features(w::Matrix)
     f_overall = w[end, :]
-    f_componets = sum(w[1:end-1, :]; dims=1)
-    return dot(f_overall, f_componets) / (norm(f_overall) * norm(f_componets))
+    f_componets = vec(sum(w[1:end-1, :]; dims=1))
+    return cosine_similarity(f_overall, f_componets)
 end
+
+function layerwise_similarity(weights::Vector{W}, feature::Int) where {W<:Matrix{T}} where {T}
+    N = length(weights)
+    s = Matrix{T}(undef, N, N)
+    for I in CartesianIndices(s)
+        s[I] = cosine_similarity(weights[I[1]][feature, :], weights[I[2]][feature, :])
+    end
+    return s
+end
+
