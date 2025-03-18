069de2766bed17d8ea661e7dd7d72fc5e736e3ce
Anoushka Bhutani <abhutani@umich.edu>
2025-03-18 13:49:19 -0700

    fix: convert targets to list for iterable dataset

--- electrolyte_fm/data_modules/tmqm_dataset.py
+++ electrolyte_fm/data_modules/tmqm_dataset.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from datasets import IterableDatasetDict, load_dataset
+import torch
 
 from .property_prediction_dataset import PropertyPredictionDataModule
 
@@ -25,3 +26,17 @@ class tmQMDataModule(PropertyPredictionDataModule):
         )
         assert isinstance(ds, IterableDatasetDict)
         return ds
+    
+    def collate_fn(self, batch):
+        for i in range(len(batch)):
+            batch[i]["target"] = batch[i]["target"].tolist()
+            batch[i]["target_mask"] = batch[i]["target_mask"].tolist()
+        output = self.token_collator(batch)
+        if self.target_columns:
+            output["target"] = torch.stack([torch.tensor(x["target"]) for x in batch])
+            output["target_mask"] = torch.stack(
+                [torch.tensor(x["target_mask"]) for x in batch]
+            )
+            assert output["target"].shape == output["target_mask"].shape
+
+        return output 
