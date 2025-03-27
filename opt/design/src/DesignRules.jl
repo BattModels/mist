2012efc61706440e72b3f139451471007876cf18
Alexius Wadell <awadell@gmail.com>
2025-03-27 10:57:49 -0400

    refactor: Move MistStyle into a proper package

--- opt/design/src/DesignRules.jl
+++ opt/design/src/DesignRules.jl
@@ -1,20 +1,20 @@
 module DesignRules
 
-using PythonCall: PythonCall, Py, PyList, pyconvert
+using PythonCall: PythonCall, Py, PyList, pyconvert, @pyconst
 using DataFrames
 using Makie
 using Statistics: Statistics, mean, std
-using StatsBase: StatsBase, stderror, mean_and_std
+using StatsBase: StatsBase, stderror, mean_and_std, mean
+using MISTStyle: MISTStyle, label, cb_attrs, ErrorCross
+using CategoricalArrays: levelcode, categorical
 
-export labels, cb_attrs
+const HARTREE_TO_EV = 27.211_386_245_981
 
 include("uq.jl")
 include("inference.jl")
 
 include("hydrocarbons.jl")
 include("plot_utils.jl")
-
-# Helper for getting the label from plot elements
-labels(x) = x.label[]
+include("trends.jl")
 
 end
