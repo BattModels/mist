30eadc7911d28da8b9af876562b329fa7c02c06d
Alexius Wadell <awadell@gmail.com>
2025-04-09 22:27:55 -0400

    fix units and quadrant

--- opt/design/src/pubchem.jl
+++ opt/design/src/pubchem.jl
@@ -1,9 +1,9 @@
 function pubchem_from_jsonl(file::String)
     rows = []
+    span(x) = maximum(x) - minimum(x)
     open(file) do f
         while !eof(f)
             mol = JSON.parse(readline(f))
-            min_lowdin = Inf
             lowdin = Float64[]
             mulliken = Float64[]
             for atom in mol["atoms"]
@@ -16,8 +16,10 @@ function pubchem_from_jsonl(file::String)
                 smi=mol["smi"],
                 min_lowdin=minimum(lowdin),
                 min_mulliken=minimum(mulliken),
-                range_lowdin=-(extrema(lowdin)...),
-                range_mulliken=-(extrema(mulliken)...),
+                max_lowdin=maximum(lowdin),
+                max_mulliken=maximum(mulliken),
+                range_lowdin=span(lowdin),
+                range_mulliken=span(mulliken),
             ))
         end
     end
