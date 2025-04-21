3b1a4aea313e9eb404a6890b7c993c58db0d604d
Alexius Wadell <awadell@gmail.com>
2025-04-21 10:43:54 -0700

    add linear probe submission and analysis scripts

--- electrolyte_fm/models/linear_probe.py
+++ electrolyte_fm/models/linear_probe.py
@@ -182,14 +182,9 @@ if __name__ == "__main__":
     import smirk
     import json
     from jsonargparse import lazy_instance
-    from lightning.pytorch.cli import (
-        LightningCLI,
-        LightningArgumentParser,
-        _InstantiatorFn,
-        _get_module_type,
-    )
     from lightning.pytorch.loggers import WandbLogger
     from lightning.pytorch.callbacks import ModelCheckpoint
+    from electrolyte_fm.utils.cli import MistLightningCLI
 
     logging.basicConfig(level=logging.INFO)
 
@@ -200,37 +195,7 @@ if __name__ == "__main__":
             name_or_path, trust_remote_code=True
         )
 
-    class MyLightningCLI(LightningCLI):
-        def add_arguments_to_parser(self, parser: LightningArgumentParser):
-            parser.add_argument(
-                "--tags",
-                type=list,
-                help="Tags for WandB logger",
-                default=[],
-            )
-            parser.link_arguments("tags", "trainer.logger.init_args.tags")
-
-        def _add_instantiators(self) -> None:
-            self.config_dump = json.loads(
-                self.parser.dump(
-                    self.config, skip_link_targets=False, skip_none=False, format="json"
-                )
-            )
-            if "subcommand" in self.config:
-                self.config_dump = self.config_dump[self.config.subcommand]
-
-            self.parser.add_instantiator(
-                _InstantiatorFn(cli=self, key="model"),
-                _get_module_type(self._model_class),
-                subclasses=self.subclass_mode_model,
-            )
-            self.parser.add_instantiator(
-                _InstantiatorFn(cli=self, key="data"),
-                _get_module_type(self._datamodule_class),
-                subclasses=self.subclass_mode_data,
-            )
-
-    cli = MyLightningCLI(
+    cli = MistLightningCLI(
         LightningProbe,
         save_config_callback=None,
         seed_everything_default=42,
--- opt/FeatureMiner/submit_explore.sh
+++ opt/FeatureMiner/submit_explore.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=explore
-#SBATCH -p cpuq
-#SBATCH -N 1
-#SBATCH -n 16
-#SBATCH -c 4
-#SBATCH --mem-per-cpu 1800M
+for ckpt in $(find ../../linear-probes/ -maxdepth 1 -mindepth 1 -type d); do
+    if [ ! -f "${ckpt}/linear_probes.jld2" ]; then
+        echo "Submitting ${ckpt}"
+        sbatch ./run_explore.sh "${ckpt}"
+    fi
+done
 
-GIT_ROOT=$(git rev-parse --show-toplevel)
-MODEL_DIR=../../linear-probes/
-source "${GIT_ROOT}/activate"
-# julia --project --startup-file=no -e 'using Pkg; Pkg.instantiate()'
-find $MODEL_DIR -maxdepth 1 -mindepth 1 -print0 | \
-    xargs -0 -P ${SLURM_NTASKS} -i ./explore_probes.jl {}
-
-# Archive results
-find $MODEL_DIR -name '*.jld2' -printf '%P\n' | \
-    tar -caf linear_probes.tar.xz -C $MODEL_DIR --files-from=-
