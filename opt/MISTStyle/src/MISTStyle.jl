e314832d455deb73e555d9ff72116cf14f5640ab
Alexius Wadell <awadell@gmail.com>
2025-04-23 10:57:43 -0400

    add BoxPlot config to style

--- opt/MISTStyle/src/MISTStyle.jl
+++ opt/MISTStyle/src/MISTStyle.jl
@@ -169,6 +169,9 @@ function theme()
             markersize=5pt,
             marker=:x,
         ),
+        BoxPlot=(;
+            markersize=4pt,
+        ),
         ErrorLines=(;
             whiskerwidth=3,
         ),
