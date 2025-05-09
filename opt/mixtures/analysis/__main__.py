918227179c77e8de04324d64fad5d2e200bccaca
Anoushka Bhutani <abhutani@umich.edu>
2025-05-09 13:38:13 -0400

    fix: move python plot formatting to common .matplotlibrc

--- opt/mixtures/analysis/__main__.py
+++ opt/mixtures/analysis/__main__.py
@@ -1,4 +1,4 @@
-from ternary_plots import generate_dataset, run_inference, plot_vector_fields
+from ternary_plots import plot_vector_fields
 import glob
 
 
@@ -7,21 +7,21 @@ if __name__ == "__main__":
     pretrained_ckpt = (
         f"/home/abhutani/electrolyte-fm/mist/{run_id}/checkpoints/last.ckpt"
     )
-    for target in ["transferance", "conductivity"]:
-        for salt_composition in [0.05, 0.1, 0.15]:
-            for salt in ["LiPF6", "TFSI"]:
-                dir_name = f"{salt}_{int(salt_composition*100)}"
-                generate_dataset(
-                    salt_mole_fraction=salt_composition, save_dir=dir_name, salt=salt
-                )
-                run_inference(
-                    pretrained_ckpt,
-                    data_dir=dir_name,
-                    val_batch_size=165,
-                    target=target,
-                )
+    # for target in ["transferance", "conductivity"]:
+    #     for salt_composition in [0.05, 0.1, 0.15]:
+    #         for salt in ["LiPF6", "TFSI"]:
+    #             dir_name = f"{salt}_{int(salt_composition*100)}"
+    #             generate_dataset(
+    #                 salt_mole_fraction=salt_composition, save_dir=dir_name, salt=salt
+    #             )
+    #             run_inference(
+    #                 pretrained_ckpt,
+    #                 data_dir=dir_name,
+    #                 val_batch_size=165,
+    #                 target=target,
+    #             )
     for target in [
-        "transferance",
+        # "transferance",
         "conductivity",
         # "product",
     ]:  # "diffusioncoeff"]:
