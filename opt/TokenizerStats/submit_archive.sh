a95ed3d2007a7e7dbc35215916af8977979cd38c
Alexius Wadell <awadell@gmail.com>
2025-06-13 09:51:09 -0400

    feat: transfer tokenizer stats archive to dataden

--- opt/TokenizerStats/submit_archive.sh
+++ opt/TokenizerStats/submit_archive.sh
@@ -6,18 +6,21 @@
 set -x
 my_job_header
 
+# Locate directory with tokenizers stats
+STATS_DIR=$(realpath stats/)
+
 # Add tokenizers.json
-cp tokenizers.json stats/
+cp tokenizers.json $STATS_DIR
 
 # Compress stats
 export XZ_OPT="-9 -v --extreme --memlimit=4000000000 --threads=${SLURM_CPUS_PER_TASK}"
 stats_archive="$(realpath ~/scratch)/tokenizer_stats_$(date +"%d%m%Y").tar.xz"
-tar -cavf  $stats_archive \
+tar -cavf  "$stats_archive" \
     --xz \
     --exclude="*.slurm" \
     --exclude="*.tmp" \
-    --exclude='*/.unmerged/*'
-    -C stats \
+    --exclude='*/.unmerged/*' \
+    -C "$STATS_DIR" \
     .
 mkdir -p ./archive
 ln -sf $stats_archive ./archive/ngram_tokenizer_stats.tar.xz
@@ -34,3 +37,13 @@ ln -sf $code_archive ./archive/TokenizerStats.tar.xz
 SMIRK_VERSION="v0.1.1"
 curl -L https://github.com/BattModels/smirk/archive/refs/tags/$(SMIRK_VERSION).tar.gz \
     --output "./archive/smirk_${SMIRK_VERSION}.tar.gz"
+
+# Archive to DataDen
+SRC="3242c149-a2b9-4dba-9406-ae3717981621" # Artemis
+DST="ab65757f-00f5-4e5b-aa21-133187732a01" # DataDen
+if [[ $(type -t globus) ]]; then
+    find $(realpath ./archive) -not -type d -printf '%f %f\n' | \
+        globus transfer --batch - \
+        $SRC:$(realpath ./archive) \
+        $DST:"/coe-venkvis/awadell/smirk-paper-archive_$(date +"%d%m%Y")/"
+fi
