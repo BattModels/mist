1a3ae3c1ebdba11d4c00a9ec4ea0bb7bde1c88da
Anoushka Bhutani <abhutani@umich.edu>
2025-06-26 16:44:16 -0400

    fix: enforce sub1=sub2 for difference

--- electrolyte_fm/models/polynomial_task_head.py
+++ electrolyte_fm/models/polynomial_task_head.py
@@ -257,6 +257,10 @@ class BezierFourthPredictionTaskHead(PolynomialPredictionTaskHead):
         # Property values at 2 control points closer to x_B = 0
         P2 = self.mlp(embedding_BA)
 
+        if self.fusion_strategy is FusionStrategy.DIFFERENCE:
+            P1 = self.mlp(embedding_AB) - self.mlp(torch.zeros_like(embedding_AB))
+            P1 = self.mlp(embedding_BA) - self.mlp(torch.zeros_like(embedding_BA))
+
         batch_size = embedding_BA.size()[0]
         P = torch.tile(torch.zeros_like(self.parametric_var), (batch_size, 1, 1))
         P[:, 1, :] = P1[:, 0].unsqueeze(1)
