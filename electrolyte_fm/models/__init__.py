f3ce150c0d2fb8aab47372e6b2b9fca97e12628f
Alexius Wadell <awadell@gmail.com>
2025-07-02 11:53:08 -0400

    Don't add SAE to model __init__.py
    Slowly breaking the import everything pattern

--- electrolyte_fm/models/__init__.py
+++ electrolyte_fm/models/__init__.py
@@ -7,4 +7,3 @@ from .prod_finetune import MISTFinetuned, MISTMultiTask
 from .roberta_base import RoBERTa
 from .roberta_prelayernorm import RoBERTaPreLayerNorm
 from .roformer_base import RoFormer
-from .sae import LightningSAE
