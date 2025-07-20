d51045d450026fdc65bbbb2a2987f1ad2806d058
Anoushka Bhutani <abhutani@umich.edu>
2025-07-20 14:32:02 -0400

    feat: temperature trend figures

--- opt/mixtures/panel.jl
+++ opt/mixtures/panel.jl
@@ -8,27 +8,19 @@ using LaTeXStrings
 
 function mixture_panel()
     df_excess = DataFrame(CSV.File("panel/excess/parity_diqlnysx.csv"))
+    df_enthalpy = DataFrame(CSV.File("panel/enthalpy/parity_ulqfugc4.csv"))
     df_ionic = DataFrame(CSV.File("panel/ionic/parity_w8phe03q.csv"))
     filter!(row -> row.predicted < 5 , df_ionic)
     filter!(row -> row.predicted > -5 , df_ionic)
-    df_list = [df_excess, df_ionic]
+    df_list = [df_excess,df_enthalpy, df_ionic]
 
     # Create figure with desired physical
-    fig = Figure(; size=(4.4606299inch, 3.50394inch))
-    cb = Colorbar(fig[:,3];
-        label = L"Temperature [K]$$",
-        colormap = MISTStyle.CONTINUOUS_COLORS,
-        colorrange = (240, 340),
-        vertical = true,
-        tellheight = false,
-        tellwidth = true,
-        flipaxis = false,
-    )
+    fig = Figure(figure_padding = 10; size=(4.4606299inch, 3.50394inch))
 
-    scatter_xlabel = ["Actual [%]", "Actual [mS/cm]"]
-    scatter_ylabel = ["MIST [%]", "MIST [mS/cm]"]
-    # 2×2 grid of equally‐sized axes
-    for col in 1:2
+    scatter_xlabel = [L"\textrm{Actual }[cm^3/mol]",L"\textrm{Actual }[J/mol]", L"\textrm{Actual }[mS/cm]"]
+    scatter_ylabel = [L"\textrm{MIST }[cm^3/mol]", L"\textrm{MIST }[J/mol]", L"\textrm{MIST }[mS/cm]"]
+
+    for col in 1:3
         df = df_list[col]
         ax = Axis(fig[1, col];
             xlabel=scatter_xlabel[col],
@@ -41,10 +33,17 @@ function mixture_panel()
         scatter!(ax, df.actual, df.predicted;
             color=df.temperature
         )
-        errorbars!(ax, df.actual, df.predicted, df.st_dev; color = df.temperature)
+        # errorbars!(ax, df.actual, df.predicted, df.st_dev; color = df.temperature)
     end
-
+    cb = Colorbar(fig[3,:];
+        label = L"Temperature [K]$$",
+        colormap = MISTStyle.CONTINUOUS_COLORS,
+        colorrange = (240, 340),
+        flipaxis = false,
+        vertical=false
+    )
     plot_excess_molar_volume!(fig, cb)
+    plot_excess_molar_enthalpy!(fig, cb)
     plot_ionic_conductivity!(fig, cb)
 
     return fig
@@ -62,26 +61,27 @@ function plot_excess_molar_volume!(fig, cb::Colorbar)
     # Create axis
     ax = Axis(fig[2, 1];
         xlabel              = L"x_1",
-        ylabel              = L"% V_m [cm^3/mol]",
+        ylabel              = L"V_m [cm^3/mol]",
         xlabelvisible       = true,
         xticksvisible       = true,
         xticklabelsvisible  = true,
+        limits = ((nothing, nothing), (-1.1, 0.2))
     )
 
     # Define a distinct marker for each (smi1, smi2) pair
     pairs = collect(Iterators.product(unique(df.smi1), unique(df.smi2)))
-    marker_list = [:circle, :square, :diamond, :utriangle, :dtriangle, :hexagon, :cross, :xcross]
+    marker_list = [:circle, :rect, :diamond, :utriangle, :dtriangle, :hexagon, :cross, :xcross]
     marker_map = Dict(p => marker_list[mod1(i, length(marker_list))] for (i, p) in enumerate(pairs))
-
+    temp_count = 0
     # Plot lines and scatter per temperature, per pair
     for (smi1, smi2) in pairs
+
         # filter inference and reference
         sub_df     = filter(row -> row.smi1 == smi1 && row.smi2 == smi2, df)
         sub_df_ref = filter(row -> row.smi1 == smi1 && row.smi2 == smi2, df_ref)
 
         mk = marker_map[(smi1, smi2)]
-        label = "$(smi1)-$(smi2)"
-
+        if temp_count%7 == 0
         for g in groupby(sub_df, :temperature)
             lines!(ax, g.x1, g.predicted_mean;
                 color = g.temperature,
@@ -89,17 +89,79 @@ function plot_excess_molar_volume!(fig, cb::Colorbar)
             )
         end
         for g in groupby(sub_df_ref, :temperature)
-            h = scatter!(ax, g.x1, g[!,"percent excess molar volume"];
+                h = scatter!(ax, g.x1, g[!,"excess_molar_volume/(cm3/mol)"];
                 color      = g.temperature,
                 marker     = mk,
-                label      = "$(smi1)-$(smi2)",
+                    label      = "$(smi1) & $(smi2)",
                 MISTStyle.cb_attrs(cb, Lines)...,
             )
+
+            end
+            temp_count += 1
+        else
+            temp_count += 1
+            continue
         end
     end
+    axislegend(ax, merge = true, unique = true, position = :rb, nbanks=1, padding=(1, 1, 1, 1),
+    margin=(1, 1, 1, 1))
     return fig
 end
 
+function plot_excess_molar_enthalpy!(fig, cb::Colorbar)
+    # Load data
+    df     = DataFrame(CSV.File("panel/enthalpy/inference.csv"))
+    df_ref = DataFrame(CSV.File("panel/enthalpy/test.csv"))
+
+    # Create axis
+    ax = Axis(fig[2, 2];
+        xlabel              = L"x_1",
+        ylabel              = L"H_m [cm^3/mol]",
+        xlabelvisible       = true,
+        xticksvisible       = true,
+        xticklabelsvisible  = true,
+        limits = ((nothing, nothing), (-1000, nothing))
+    )
+
+    # Define a distinct marker for each (smi1, smi2) pair
+    pairs = collect(Iterators.product(unique(df.smi1), unique(df.smi2)[1:10]))
+    marker_list = [:circle, :star5, :rect, :diamond, :ltriangle, :utriangle, :hline, :rtriangle,:vline, :star4, :pentagon, :dtriangle, :hexagon, :cross, :xcross]
+    marker_map = Dict(p => marker_list[mod1(i, length(marker_list))] for (i, p) in enumerate(pairs))
+    temp_count = 0
+    # Plot lines and scatter per temperature, per pair
+    for (smi1, smi2) in pairs
+
+        # filter inference and reference
+        sub_df     = filter(row -> row.smi1 == smi1 && row.smi2 == smi2, df)
+        sub_df_ref = filter(row -> row.smi1 == smi1 && row.smi2 == smi2, df_ref)
+
+        mk = marker_map[(smi1, smi2)]
+        if temp_count%1 == 0
+        for g in groupby(sub_df, :temperature)
+            lines!(ax, g.x1, g.predicted_mean;
+                color = g.temperature,
+                MISTStyle.cb_attrs(cb, Lines)...,
+            )
+        end
+        for g in groupby(sub_df_ref, :temperature)
+                h = scatter!(ax, g.x1, g[!,"excess_molar_enthalpy/(J/mol)"];
+                color      = g.temperature,
+                marker     = mk,
+                    label      = "$(smi2)",
+                MISTStyle.cb_attrs(cb, Lines)...,
+            )
+
+            end
+            temp_count += 1
+        else
+            temp_count += 1
+            continue
+        end
+    end
+    axislegend(ax, merge = true, unique = true, position = :rb, nbanks=2, padding=(1, 1, 1, 1),
+    margin=(1, 1, 1, 1))
+    return fig
+end
 
 function plot_ionic_conductivity!(fig, cb::Colorbar)
     csv_path = "panel/ionic/ionic_conductivity_curves.csv"
@@ -112,19 +174,18 @@ function plot_ionic_conductivity!(fig, cb::Colorbar)
     # Sort temperatures and build a continuous, dark‐cropped colormap
     temps = sort(unique(df.temperature))
     axes = [
-        Axis(fig[1 + i, 2];
-            title            = salt,
-            xlabel           = i==2 ? L"x_{Li}" : "",    # only show x‐label on bottom panels
-            ylabel           = i==1 ? L"\sigma\ [mS/cm]" : "",
-            xticks           = (0:0.05:0.2, ["0.0", "0.05","0.1","0.15","0.2"]),
-            yticks           = (0:2:6, ["0","2","4","6"]),
+        Axis(fig[1 + i, 3];
+            xlabel           = L"x_{Li}",    # only show x‐label on bottom panels
+            ylabel           = L"\sigma\ [mS/cm]",
+            # xticks           = (0:0.05:0.2, ["0.0", "0.05","0.1","0.15","0.2"]),
+            # yticks           = (0:2:6, ["0","2","4","6"]),
         )
         for (i, salt) in enumerate(salts)
     ]
 
     # Plot each temperature curve in both panels
     for ax in axes, T in temps
-        sol = ax.title[]
+        sol = "LiTFSI"
         mask = (df.salt_name .== sol) .& (df.temperature .== T)
         sub = df[mask, :]
         lines!(
