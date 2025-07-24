3fd19c1cc6060abe10d88736ca1d994b0ffb4bc2
Anoushka Bhutani <abhutani@umich.edu>
2025-07-24 12:02:06 -0400

    feat: SOAP figures

--- opt/mixtures/plot_excess.jl
+++ opt/mixtures/plot_excess.jl
@@ -332,10 +332,137 @@ function plot_excess_skewness!(f, df)
     return f
 end
 
+function plot_soap!(f)
+    x_min = 0.3
+    x_max = 1.0
+    y_min = 0.0
+    y_max = 0.035
+    sim_threshold = 0.6
+    excess_threshold = 0.025
+    ax1 = Axis(f[1, 1];
+        limits=((x_min, 1), (0, y_max)),
+        xlabel="REMatch Similarity",
+        ylabel=L"max($\left|\frac{V^{E}_m}{V_m}\right|$)",
+        yticks=WilkinsonTicks(3; k_min = 3, k_max=5)
+        # xtickformat="{:.0%}",
+        # ytickformat="{:.0%}",
+        # xlabelvisible=false,
+        # xticksvisible=false,
+        # xticklabelsvisible=false,
+    )
+    df = DataFrame(CSV.File("panel_data/soap_similarity.csv"))
+    df[!, :abs_target] = abs.(
+        df[!,:"excess molar volume [centimeter ** 3 / mole]"] ./
+        df[!,:"molar volume [centimeter ** 3 / mole]"]
+    )
+    df = transform(
+    groupby(df, [:smi1, :smi2]),
+    :abs_target => maximum => :max_abs_relative_vol
+    )
+    
+    # Shade low similarity, low excess
+    poly!(Point2f[(x_min, y_min), (x_min, y_max), (sim_threshold, y_max), (sim_threshold, y_min)], color = MISTStyle.UM_COLORS.maize, alpha = 0.2 , strokewidth = 0)
+    # Shade high similarity, high excess
+    poly!(Point2f[(sim_threshold, excess_threshold), (sim_threshold, y_max), (x_max, y_max), (x_max, excess_threshold)], color = MISTStyle.UM_COLORS.maize, alpha = 0.2 , strokewidth = 0)
+    scatter!(ax1, df[!, "similarity"],  df[!, "max_abs_relative_vol"]; color = MISTStyle.UM_COLORS.blue,)
+    dropmissing!(df, "similarity")
+    dropmissing!(df, "max_abs_relative_vol")
 
-function mixture_panel(model, df_all)
+    df_up = subset(df,
+            :similarity => ByRow(>(sim_threshold)),
+            :max_abs_relative_vol => ByRow(>(excess_threshold)),
+        )
+    filter!(row -> row.max_abs_relative_vol == row.abs_target, df_up)
+    CSV.write("panel_data/df_up.csv", df_up)
+    
+    gdf = groupby(df_up, ["smi1", "smi2", "temperature [kelvin]"])
+    transform!(df_up, ["smi1", "smi2" ] => ByRow((x, y) -> sort([x, y])) => :compound_id)
+    df_up = combine(first, groupby(df_up, :compound_id))
+    for subdf in gdf
+        name1 = subdf.smi1[1]
+        name2 = subdf.smi2[1]
+        points = Point2.(subdf.similarity[1], subdf.max_abs_relative_vol[1])
+        text!(ax1, subdf.similarity[1], subdf.max_abs_relative_vol[1], text ="$name1 & $name2", align = (:left, :top))
+    end
+
+    df_left = subset(df,
+        :similarity => ByRow(<(sim_threshold)),
+        :max_abs_relative_vol => ByRow(<(excess_threshold)),
+    )
+    filter!(row -> row.max_abs_relative_vol == row.abs_target, df_left)
+    CSV.write("panel_data/df_left.csv", df_left)
+
+    gdf = groupby(df_left, ["smi1", "smi2", "temperature [kelvin]"])
+    for subdf in gdf
+        name1 = subdf.smi1[1]
+        name2 = subdf.smi2[1]
+        text!(ax1, subdf.similarity[1], subdf.max_abs_relative_vol[1], text ="$name1& $name2")
+    end
+    return f
+end  
+
+function plot_soap_outliers!(f, model)
+    ax2 = Axis(f[1, 1];
+        xlabel=L"$x_1$",
+        ylabel=L"$V^{E}_m \; cm^3/mol$",
+    )
+    mixtures = [
+        pydict(; compounds=@py(["CC1COC(=O)O1", "ClC(Cl)Cl"]), temperature=298.15),
+        pydict(; compounds=@py(["NCCN", "OCCO"]), temperature=298.15),
+        pydict(; compounds=@py(["CN(CCO)CCO", "O"]), temperature=298.15),
+        pydict(; compounds=@py(["CN1CCN(C)C1=O", "O"]), temperature=298.15)
+    ]
+    df = Mixtures.evaluate_mixtures(model, mixtures)
+    df.x1 = first.(df.composition)
+
+    data = DataFrame(CSV.File("panel_data/soap_similarity.csv"))
+
+    for gdf in groupby(df, ["compounds"])
+        
+        @info nrow(gdf)
+        df_ref = subset(data,
+            :smi1 => ByRow(in(gdf.compounds[1])),
+            :smi2 => ByRow(in(gdf.compounds[1])),
+            "temperature [kelvin]" => ByRow(==(298.15))
+        )
+
+        @info nrow(df_ref)
+        lines!(ax2, gdf.x1, gdf.molar_volume_excess;
+        )
+
+        if nrow(df_ref)> 2
+            dropmissing!(df_ref, "excess molar volume [centimeter ** 3 / mole]")
+            scatter!(ax2, df_ref[!, "x1"], df_ref[!, "excess molar volume [centimeter ** 3 / mole]"];
+            )
+        end
+    end
+
+    vargs = (; linewidth=1.5pt, colormap=:tab10, colorrange=(1, 10))
+    elem = map(enumerate(mixtures)) do (color, spec)
+        name1 = spec["compounds"][0]
+        name2 = spec["compounds"][1]
+        label = "$name1 & $name2"
+        LineElement(; color, label, vargs...)
+    end
+
+    Legend(f[3, 1], elem, MISTStyle.label.(elem);
+        tellwidth=true,
+        tellheight=true,
+        nbanks=2,
+        halign=:center,
+        margin=2pt .* (1, 1, 1, 1),
+        orientation=:horizontal,
+    )
+    return f
+end 
+
+
+function mixture_panel(model_path, df_all)
+
+    model = pyexcess.load_excess_model(model_path)
+    
     f = Figure(; size=(89mm, 190mm))
-    plot_excess_skewness!(f[2, 2], df)
+    # plot_excess_skewness!(f[2, 2], df)
     sublabel!(f[2, 2, TopLeft()], "d"; left=15pt)
     Axis(f[4, :])
     return f
