93249b1815087e2c17a8509c8f80e11de28ba6ca
Anoushka Bhutani <abhutani@umich.edu>
2025-08-01 11:27:48 -0400

    more optimization utils

--- opt/mixtures/optimization/excess_property_optimization.py
+++ opt/mixtures/optimization/excess_property_optimization.py
@@ -1,26 +1,39 @@
+import os
+import sys
+import json
 from pathlib import Path
+from dataclasses import asdict, dataclass
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import torch
-import numpy as np
-from torch.optim import LBFGS
-from difftopk import DiffTopkNet
-from electrolyte_fm.models.model_utils import masked_mean_pool
-from smirk import SmirkTokenizerFast
 import torch.nn.functional as F
-import sys
+from difftopk import DiffTopkNet
+from smirk import SmirkTokenizerFast
+from torch.optim import LBFGS
 
 from electrolyte_fm.models.excess_physics_model import (
-    ExcessPhysicsLightningModel,
-    ExcessPhysicsModel,
-)
+    ExcessPhysicsLightningModel, ExcessPhysicsModel)
+from electrolyte_fm.models.model_utils import masked_mean_pool
+
 sys.path.append(
     Path(__file__).absolute().parent.parent.joinpath("python", "excess.py")
 )
+sys.path.append(
+    Path(__file__).absolute().parent.joinpath("utils.py")
+)
+from excess import evaluate_mixtures
+from utils import process_prediction_with_ref
 
+@dataclass
+class OptimizationConfig:
+    lr: float
+    num_iterations: int
 
 class ExcessOptimizer:
     def __init__(
-        self, pretrained_ckpt: str, temperature: float,
+        self, pretrained_ckpt: str, temperature: float, config: OptimizationConfig
     ) -> None:
         self.pretrained_ckpt = pretrained_ckpt
         self.model = self._load_model(self.pretrained_ckpt)
@@ -36,6 +49,8 @@ class ExcessOptimizer:
             steepness=10.0,
             distribution="cauchy",
         )
+        self.config = config
+        self.target_trajectory = []
 
     def _load_model(self, ckpt: str):
         if Path(ckpt).is_dir():
@@ -69,7 +84,7 @@ class ExcessOptimizer:
             E = masked_mean_pool(hs, attention_mask)  # (n, E)
         return E
 
-    def cost(self, x, c_logits):
+    def cost(self, x: torch.Tensor, c_logits: torch.Tensor):
         # soft top‑2 selection
         _, S = self.sorter(x.unsqueeze(0))  # S: (1, n, 2)
         S = S.squeeze(0)  # (n, 2)
@@ -82,10 +97,11 @@ class ExcessOptimizer:
         comp = F.softmax(c_logits, dim=0).view(1, 2)  # (1,2)
 
         y_rel = self.forward(comp, embs)
+        self.target_trajectory.append(y_rel.detach().numpy())
         mx, _ = torch.max(torch.abs(y_rel), dim=1)
         return -mx.sum()
 
-    def forward(self, comp, embs):
+    def forward(self, comp: torch.Tensor, embs: torch.Tensor):
         model = self.model
         y, _, y_excess = model.compute_interactions(comp, embs, torch.tensor(self.temperature))
         # loss = -∑_t max |y_excessₜ / yₜ|
@@ -95,9 +111,8 @@ class ExcessOptimizer:
     def run_optimization(self):
         x = torch.randn(len(self.inventory), device=device, requires_grad=True)  # inventory scores
         c_logits = torch.randn(2, device=device, requires_grad=True)  # raw mix logits
-        optimizer = LBFGS([x, c_logits], lr=1.0, max_iter=20)
+        optimizer = LBFGS([x, c_logits], lr=self.config.lr, max_iter=self.config.lr)
      
-
         def closure():
             optimizer.zero_grad()
             loss = self.cost(x, c_logits)
@@ -110,15 +125,72 @@ class ExcessOptimizer:
         optimizer.step(closure)
 
         _, top2 = torch.topk(x, k=2)
-        best_comps = [self.inventory[i] for i in top2]
-        best_fracs = F.softmax(c_logits, dim=0).tolist()
+        self.best_fracs = F.softmax(c_logits, dim=0).tolist()
+        self.optimal_mixture = [
+            {
+                "compounds": [self.inventory[i] for i in top2],
+                "temperature": self.temperature
+            },
+        ]
+        print("Mixture fractions :", self.best_fracs)
+        self.target_trajectory = np.squeeze(np.array(self.target_trajectory))
+    
+    def save_run(self):
+        run_id = f'run_{datetime.now().strftime(format="%d%m%y_%H%M")}'
+        save_path = Path(__file__).absolute().parent.joinpath("results", run_id)
+        os.makedirs(save_path, exist_ok=True)
+        # Save config
+        with open(save_path.joinpath("config.json"), 'w') as json_file:
+            json.dump(asdict(self.config), json_file, indent=4)
 
-        print("Selected compounds:", best_comps)
-        print("Mixture fractions :", best_fracs)
+        # Save optimized curves
+        out = evaluate_mixtures(model = self.model, mixtures = self.optimal_mixture)
+        df = process_prediction_with_ref(out, targets = self.model.config.target_columns)
+        df.to_csv(save_path.joinpath("optimized_curves.csv"))
+
+        # Save trajectory
+        np.savez(save_path.joinpath("trajectory.npz"), self.target_trajectory)
+
+        # Save figures
+        f_traj = self.plot_trajectory()
+        f_traj.savefig(save_path.joinpath("trajectory.png"))
+
+        f_curves = self.plot_optimization_results(df)
+        f_curves.savefig(save_path.joinpath("optimal_curves.png"))
+
+       
+    
+    def plot_trajectory(self):
+        fig, ax = plt.subplots()
+        for idx, target in enumerate(self.model.config.target_columns):
+            ax.plot(self.target_trajectory[:, idx], label = target)
+        ax.set_xlabel("Iterations")
+        ax.set_ylabel("% Excess")
+        fig.legend()
+        return fig
+
+    
+    def plot_optimization_results(self, results_df: pd.DataFrame):
+        fig, ax = plt.subplots()
+        x1 = [i[0] for i in results_df.composition]
+        for idx, target in enumerate(self.model.config.target_columns):
+            y_rel = results_df[f"relative excess {target}"]
+            ax.plot(x1, y_rel, label = target)
+        ax.set_xlabel("$x_1$")
+        ax.set_ylabel("% Excess")
+        fig.legend()
+        return fig
+
+
+        
+        
 
 
 if __name__ == "__main__":
+    
     name_or_path = "/Users/anoushka/VSCodeProjects/electrolyte-fm/data/models/mist-mixtures-17o0xho9"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    opt = ExcessOptimizer(name_or_path, temperature = 298.15)
+    config = OptimizationConfig(lr=0.1, num_iterations=30)
+    opt = ExcessOptimizer(name_or_path, temperature = 298.15, config=config)
     opt.run_optimization()
+    opt.save_run()
---
+++ opt/mixtures/optimization/utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def process_prediction_with_ref(iter, targets):
+    rows = []
+
+    for row in iter:
+        out = {
+            "compounds": row["compounds"],
+            "composition": row["composition"],
+            "temperature": row["temperature"],
+        }
+
+        for idx, target in enumerate(targets):
+            out[target] = row["y"][idx]
+            out[f"excess {target}"] = row["y_excess"][idx]
+            out[f"linear {target}"] = row["y_linear"][idx]
+            out[f"relative excess {target}"] = row["y_excess"][idx]/ (row["y_excess"][idx] + row["y_linear"][idx])
+        rows.append(out)
+
+    return pd.DataFrame(rows)    
