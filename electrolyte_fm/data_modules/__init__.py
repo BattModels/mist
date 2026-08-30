3f90eac01c7e0bb18f383edc418227a499b111ac
Anoushka Bhutani <abhutani@umich.edu>
2026-08-30 09:05:58 -0400

    import CSVDataset at top level

--- electrolyte_fm/data_modules/__init__.py
+++ electrolyte_fm/data_modules/__init__.py
@@ -1,9 +1,10 @@
-from .molnet_dataset import MolNetDataModule
-from .property_prediction_dataset import PropertyPredictionDataModule
-from .roberta_dataset import RobertaDataSet
-from .tmqm_dataset import tmQMDataModule
+from .csv_dataset import CSVDataModule
 from .mixture_dataset import (
     ComponentDataModule,
     ComponentDataModuleFast,
     MultiMixtureDataModule,
 )
+from .molnet_dataset import MolNetDataModule
+from .property_prediction_dataset import PropertyPredictionDataModule
+from .roberta_dataset import RobertaDataSet
+from .tmqm_dataset import tmQMDataModule
